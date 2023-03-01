5b39e53c1bba734343ffe24273079370b4349162
dangtung <dangtung006@gmail.com>
2023-03-01 17:24:49 +0700

    update schema

---
+++ src/models/book.ts
@@ -0,0 +1,18 @@
+import mongoose, { Document, Schema } from 'mongoose';
+import { IBook } from '../interfaces/book';
+
+
+export interface IBookModel extends IBook, Document {}
+
+const BookSchema: Schema = new Schema(
+    {
+        title: {  type: String, required: true },
+        author: { type: Schema.Types.ObjectId, required: true,  ref: 'Author' }
+    },
+    {
+        timestamps: true,
+        versionKey: false
+    }
+);
+
+export default mongoose.model<IBookModel>('Book', BookSchema);
