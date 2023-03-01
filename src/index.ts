d75477c3d270540655f0d28de99a7e7ffff72437
dangtung <dangtung006@gmail.com>
2023-03-01 16:36:49 +0700

    update middlewares

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import cors from "cors";
 import helmet from "helmet";
 import { itemsRouter } from "./item/item.router";
 import mongoose from 'mongoose';
+dotenv.config();
 
 const connectDB = async function():Promise<void> {
     try{
@@ -14,19 +15,48 @@ const connectDB = async function():Promise<void> {
             }
         )
         console.log("connect successfull");
+        const app = express();
+        const PORT: number = parseInt(process.env.PORT as string, 10);
+
+        app.use(express.urlencoded({ extended: true }));
+        app.use(express.json());
+        
+
+        // Middlewares :
+        
+        app.use((req, res, next) => {
+            // /** Log the req */
+            // Logging.info(`Incomming - METHOD: [${req.method}] - URL: [${req.url}] - IP: [${req.socket.remoteAddress}]`);
+    
+            // res.on('finish', () => {
+            //     /** Log the res */
+            //     Logging.info(`Result - METHOD: [${req.method}] - URL: [${req.url}] - IP: [${req.socket.remoteAddress}] - STATUS: [${res.statusCode}]`);
+            // });
+            next();
+        });
+
+        /** Rules of our API */
+        app.use((req, res, next) => {
+            res.header('Access-Control-Allow-Origin', '*');
+            res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    
+            if (req.method == 'OPTIONS') {
+                res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+                return res.status(200).json({});
+            }
+    
+            next();
+        });
+
+        app.use("/items", itemsRouter);
+
+        const server = app.listen(PORT, () => {
+            console.log(`Listening on port ${PORT}`);
+        });
+
     }catch(e){
         console.log("fail to connect DB" , e);
     }
 }
 
 connectDB();
-dotenv.config();
-
-const app = express();
-app.use("/items", itemsRouter);
-
-
-const PORT: number = parseInt(process.env.PORT as string, 10);
-const server = app.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}`);
-});
