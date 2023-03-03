890601b4d8ff6c074b1d5b0a9be3e8d14d662540
dangtung <dangtung006@gmail.com>
2023-03-03 13:33:30 +0700

    update error class

--- src/helper/error.helper.ts
+++ src/helper/error.helper.ts
@@ -1,10 +1,20 @@
+import { HttpStatusCode } from "../const/httpCode";
+
 export class BaseError extends Error {
     public readonly name: string;
-    // public readonly httpCode: HttpStatusCode;
+    public readonly httpCode: HttpStatusCode;
 
-    constructor(name: string){
+    constructor(name: string, httpCode: HttpStatusCode){
         super();
+        Object.setPrototypeOf(this, new.target.prototype);
         this.name = name;
-        
+        this.httpCode = httpCode;
+        Error.captureStackTrace(this);
+    }
+}
+
+export class HTTP400Error extends BaseError {
+    constructor(){
+        super("BAD REQUEST", HttpStatusCode.BAD_REQUEST);
     }
 }
