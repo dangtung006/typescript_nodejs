d15c2e2620c825f33d667f51be8dedb4301cdbf7
dangtung <dangtung006@gmail.com>
2023-03-03 15:56:00 +0700

    init passport local

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import * as dotenv from "dotenv";
-import passport from 'passport'
+import passport from 'passport';
+import session from 'express-session';
 import cors from "cors";
 import helmet from "helmet";
 import { itemsRouter } from "./item/item.router";
@@ -9,6 +10,7 @@ import bookRoutes from './route/book';
 import authorRoutes from './route/author';
 import userRoutes from './route/user';
 import passportMiddleware from './middlewares/passport.Jwt';
+import {initPassportLocal } from "./middlewares/passport.local"
 
 dotenv.config();
 
@@ -26,7 +28,19 @@ const connectDB = async function():Promise<void> {
 
         app.use(express.urlencoded({ extended: true }));
         app.use(express.json());
+
+        app.use(
+            session({
+                secret: "secretcode",
+                resave: true,
+                saveUninitialized: true,
+            })
+        );
+
         app.use(passport.initialize());
+        app.use(passport.session());
+
+        initPassportLocal();
         passport.use(passportMiddleware);
 
         
